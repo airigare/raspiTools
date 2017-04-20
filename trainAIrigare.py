7f0daa0823c91e4c46851ec2b7342669204b7592
bawdiest@raspberrypi <com@mikmak.cc>
2017-04-20 23:16:25 +0000

    Write Log to /tmp/LoPoSwitch.log

--- trainAIrigare.py
+++ trainAIrigare.py
@@ -4,6 +4,15 @@ import pexpect, sys, json, select, MySQLdb
 from time import time
 #from sensor_calcs import *
 # LoPoSwitch
+
+# function to transform hex string like "0a cd" into signed integer
+def hexStrToInt(hexstr):
+    val = int(hexstr[0:2],16) + (int(hexstr[3:5],16)<<8)
+    if ((val&0x8000)==0x8000): # treat signed 16bits
+        val = -((val^0xffff)+1)
+    return val
+
+
 def floatfromhex(h):
     t = float.fromhex(h)
     if t > float.fromhex('7FFF'):
@@ -12,8 +21,11 @@ def floatfromhex(h):
     return t
 
 class LoPoSwitch:
+	file = "/tmp/LoPoSwitch.log"
+
 	def __init__( self, bluetooth_adr ):
 		self.con = pexpect.spawn('gatttool -b ' + bluetooth_adr + ' --interactive -t random --listen')
+		self.con.logfile = open(self.file, "a")
 		self.con.expect('\[LE\]>', timeout=100)
 		print "Preparing to connect."
 		self.con.sendline('connect')
@@ -23,28 +35,37 @@ class LoPoSwitch:
 		#self.con.expect('\[CON\].*>')
 		self.cb = {}
 		self.con.sendline('char-write-req 0x000e 0100')
+		self.con.expect('Characteristic value was written successfully')
 		self.cb = {}
 		return
 
 	def turnOn(self):
 		self.con.sendline('connect') # Reconnect if not connected
+		self.cb = {}
+                self.con.sendline('char-write-req 0x000e 0100')
+		self.con.expect('Characteristic value was written successfully')
+
 		cmd = 'char-write-cmd 0x000b 5231' #Write 'R1' to second attribute
-		print cmd
+		#print cmd
 		self.con.sendline( cmd )
-		print(child.before)
+		self.con.expect("\[LE\].*>")
 		self.cb = {}
 		return
 
 	def turnOff(self):
 		self.con.sendline('connect') # Reconnect if not connected
-
+                self.cb = {}
+                self.con.sendline('char-write-req 0x000e 0100')
+                self.con.expect('Characteristic value was written successfully')
+		
 		cmd = 'char-write-cmd 0x000b 5230' #Write 'R0' to second attribute
-		print cmd
+		#print cmd
 		self.con.sendline( cmd )
-		print(child.before)
+		self.con.expect("\[LE\].*>")
+		self.cb = {}
 		return
 
-	def writeLog(t):
+	def writeLog():
 		print("Data logged successefully")
 		return -1
 
@@ -71,7 +92,7 @@ while True:
     Pump.turnOff()
     wT = time() - sT
     print("I was watering " + str(round(wT,0)) + " seconds")
-    writeLog(wT)
+    #Pump.writeLog(wT)
 
 #if __name__ == "__main__":
 #    main()
