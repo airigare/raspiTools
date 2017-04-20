0a8d7ad8218d3fa3e60e7771dead2fea2cad9c90
bawdiest@raspberrypi <com@mikmak.cc>
2017-04-20 20:00:19 +0000

    Correct Idents

--- trainAIrigare.py
+++ trainAIrigare.py
@@ -12,7 +12,6 @@ def floatfromhex(h):
     return t
 
 class LoPoSwitch:
-
 	def __init__( self, bluetooth_adr ):
 		self.con = pexpect.spawn('gatttool -b ' + bluetooth_adr + ' --interactive -t random --listen')
 		self.con.expect('\[LE\]>', timeout=100)
@@ -25,31 +24,29 @@ class LoPoSwitch:
 		self.cb = {}
 		self.con.sendline('char-write-req 0x000e 0100')
 		self.cb = {}
-	return
-
-	def turnOn(self):
-    	self.con.sendline('connect') # Reconnect if not connected
-    	
-        cmd = 'char-write-cmd 0x000b 5231' #Write 'R1' to second attribute
-        print cmd
-        self.con.sendline( cmd )
-        print(child.before)
-        self.cb = {}
 		return
 
-    def turnOff(self):
-    	self.con.sendline('connect') # Reconnect if not connected
-    	
-        cmd = 'char-write-cmd 0x000b 5230' #Write 'R0' to second attribute
-        print cmd
-        self.con.sendline( cmd )
-        print(child.before)
-        return
+	def turnOn(self):
+		self.con.sendline('connect') # Reconnect if not connected
+		cmd = 'char-write-cmd 0x000b 5231' #Write 'R1' to second attribute
+		print cmd
+		self.con.sendline( cmd )
+		print(child.before)
+		self.cb = {}
+		return
 
-def writeLog(t):
+	def turnOff(self):
+		self.con.sendline('connect') # Reconnect if not connected
 
-	print("Data logged successefully")
-	return -1
+		cmd = 'char-write-cmd 0x000b 5230' #Write 'R0' to second attribute
+		print cmd
+		self.con.sendline( cmd )
+		print(child.before)
+		return
+
+	def writeLog(t):
+		print("Data logged successefully")
+		return -1
 
 
 
